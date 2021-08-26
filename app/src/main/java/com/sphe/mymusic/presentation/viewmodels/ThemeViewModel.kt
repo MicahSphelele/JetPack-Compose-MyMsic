4868f26eb44bb4ac0200dd4d2cf9d11ac0c67824
Sphelele <sphemicah@gmail.com>
2021-08-26 18:12:18 +0200

    - Added ui-components module  💻

--- app/src/main/java/com/sphe/mymusic/presentation/viewmodels/ThemeViewModel.kt
+++ app/src/main/java/com/sphe/mymusic/presentation/viewmodels/ThemeViewModel.kt
@@ -1,11 +1,10 @@
 package com.sphe.mymusic.presentation.viewmodels
 
 import androidx.lifecycle.ViewModel
-import com.sphe.mymusic.util.storage.PreferencesStore
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class ThemeViewModel @Inject constructor(private val preferences: PreferencesStore) : ViewModel() {
+class ThemeViewModel @Inject constructor() : ViewModel() {
     //val themeState = preferences.get(AppConstants.THEME_STATE_KEY, ThemeState.serializer(), null)
 }
