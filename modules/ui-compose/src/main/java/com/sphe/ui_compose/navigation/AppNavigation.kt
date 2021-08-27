b4376a3edc35ab242cde8119f02105259b70f5aa
Sphelele <sphemicah@gmail.com>
2021-08-27 14:36:51 +0200

    - Work not completed on AppNavigation  💻

--- modules/ui-compose/src/main/java/com/sphe/ui_compose/navigation/AppNavigation.kt
+++ modules/ui-compose/src/main/java/com/sphe/ui_compose/navigation/AppNavigation.kt
@@ -2,6 +2,9 @@ package com.sphe.ui_compose.navigation
 
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.navigation
+import com.sphe.ui_compose.collectEvent
 import kotlinx.coroutines.InternalCoroutinesApi
 
 @OptIn(InternalCoroutinesApi::class)
@@ -10,5 +13,20 @@ internal fun AppNavigation(
     navController: NavController,
     navigator: Navigator = LocalNavigator.current
 ) {
+    collectEvent(flow = navigator.queue) { event ->
+        when(event) {
+            is NavigationEvent.Destination -> navController.navigate(event.route)
+            is NavigationEvent.Back -> navController.navigateUp()
+            else -> Unit
+        }
+    }
+}
 
+private fun NavGraphBuilder.addSongsRoot(navController: NavController) {
+    this.navigation(
+        route = RootScreen.SongScreen.route,
+        startDestination = LeafScreen.SongScreen.route
+    ) {
+
+    }
 }
