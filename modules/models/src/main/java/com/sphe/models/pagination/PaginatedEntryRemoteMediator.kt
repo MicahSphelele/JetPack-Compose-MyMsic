cf1c7c92e652f6e74e91d0ba53b4de02e966546f
Sphelele <sphemicah@gmail.com>
2021-09-13 08:23:54 +0200

    - Completed PaginatedEntryRemoteMediator  💻

--- modules/models/src/main/java/com/sphe/models/pagination/PaginatedEntryRemoteMediator.kt
+++ modules/models/src/main/java/com/sphe/models/pagination/PaginatedEntryRemoteMediator.kt
@@ -1,4 +1,34 @@
 package com.sphe.models.pagination
 
-internal class PaginatedEntryRemoteMediator<Params: Any, E>(private val fetch: suspend (page: Int, refreshing: Boolean) -> Unit) {
+import androidx.paging.ExperimentalPagingApi
+import androidx.paging.LoadType
+import androidx.paging.PagingState
+import androidx.paging.RemoteMediator
+
+@OptIn(ExperimentalPagingApi::class)
+internal class PaginatedEntryRemoteMediator<Params: Any, E>(private val fetch: suspend (page: Int, refreshing: Boolean) -> Unit
+) : RemoteMediator<Params, E>() where E : PaginatedEntity {
+
+    override suspend fun load(loadType: LoadType, state: PagingState<Params, E>): MediatorResult {
+        val nextPage = when(loadType) {
+            LoadType.REFRESH -> 0
+            LoadType.PREPEND -> return MediatorResult.Success(endOfPaginationReached = true)
+            LoadType.APPEND -> {
+                val lastItem = state.lastItemOrNull() ?: return MediatorResult.Success(endOfPaginationReached = true)
+                lastItem.page + 1
+            }
+        }
+
+        return try {
+            // we're assuming if the are already pages and loadType is refreshing, then user must be refreshing it manually
+            // which can be used to force refresh the data source
+            val isRefreshing = loadType == LoadType.REFRESH && state.pages.isNotEmpty()
+            fetch(nextPage, isRefreshing)
+            MediatorResult.Success(endOfPaginationReached = false)
+        }catch (t: Throwable) {
+            MediatorResult.Error(t)
+        }
+    }
+
+
 }
