2c6d7c478d9c7f79dfc72a99da3afdcc4853c209
Sphelele <sphemicah@gmail.com>
2021-09-17 11:34:34 +0200

    - Updated ThemeViewModel  💻

--- modules/ui-theme/src/main/java/com/sphe/ui_theme/ThemeViewModel.kt
+++ modules/ui-theme/src/main/java/com/sphe/ui_theme/ThemeViewModel.kt
@@ -2,9 +2,13 @@ package com.sphe.ui_theme
 
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.sphe.base.storge.PreferenceKeys
 import com.sphe.base.storge.PreferencesStore
 import com.sphe.base.ui.ThemeState
+import com.sphe.ui_theme.theme.DefaultTheme
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -12,11 +16,12 @@ class ThemeViewModel @Inject constructor(
     private val handle: SavedStateHandle,
     private val preferences: PreferencesStore,
 ) : ViewModel() {
-    //val themeState = preferences.get(PreferenceKeys.THEME_STATE_KEY, ThemeState.serializer(), DefaultTheme)
+
+    val themeState = preferences.get(PreferenceKeys.THEME_STATE_KEY, ThemeState.serializer(), DefaultTheme)
 
     fun applyThemeState(themeState: ThemeState) {
-//        viewModelScope.launch {
-//            preferences.save(PreferenceKeys.THEME_STATE_KEY, themeState, ThemeState.serializer())
-//        }
+        viewModelScope.launch {
+            preferences.save(PreferenceKeys.THEME_STATE_KEY, themeState, ThemeState.serializer())
+        }
     }
 }
