d33cf603cb388e4bd21b59c443b7af64679debdf
Sphelele <sphemicah@gmail.com>
2021-09-21 07:11:15 +0200

    - Created QueueHelper  💻

--- modules/data/src/main/java/com/sphe/models/db/QueueHelper.kt
+++ modules/data/src/main/java/com/sphe/models/db/QueueHelper.kt
@@ -1,2 +1,29 @@
 package com.sphe.models.db
 
+import com.sphe.models.db.dao.QueueDao
+import com.sphe.models.db.entities.QueueEntity
+import com.sphe.models.repositories.SongsRepository
+
+interface QueueHelper {
+
+    fun updateQueueSongs(
+        queueSongs: LongArray?,
+        currentSongId: Long?
+    )
+
+    fun updateQueueData(queueData: QueueEntity)
+}
+
+class RealQueueHelper(
+    private val queueDao: QueueDao,
+    private val songsRepository: SongsRepository
+) : QueueHelper {
+    override fun updateQueueSongs(queueSongs: LongArray?, currentSongId: Long?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun updateQueueData(queueData: QueueEntity) {
+        TODO("Not yet implemented")
+    }
+
+}
