242b85cd35e287079cefebb0127536a37b2f95f4
Sphelele <sphemicah@gmail.com>
2021-09-21 06:35:31 +0200

    - Created SongEntity  💻

---
+++ modules/data/src/main/java/com/sphe/models/db/entities/SongEntity.kt
@@ -0,0 +1,11 @@
+package com.sphe.models.db.entities
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "queue_songs")
+data class SongEntity(
+    @PrimaryKey(autoGenerate = true) var uid: Int? = null,
+    @ColumnInfo(name = "id") var id: Long
+)
