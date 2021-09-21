041622f24bf7426654b05431f682757e8089ed6b
Sphelele <sphemicah@gmail.com>
2021-09-21 07:09:18 +0200

    - Updated MyMusicDatabase  💻

--- modules/data/src/main/java/com/sphe/models/db/MyMusicDatabase.kt
+++ modules/data/src/main/java/com/sphe/models/db/MyMusicDatabase.kt
@@ -1,12 +1,13 @@
 package com.sphe.models.db
 
 import androidx.room.Database
+import androidx.room.RoomDatabase
 import com.sphe.models.db.dao.QueueDao
 import com.sphe.models.db.entities.QueueEntity
 import com.sphe.models.db.entities.SongEntity
 
 @Database(entities = [QueueEntity::class, SongEntity::class], version = 1, exportSchema = false)
-abstract class MyMusicDatabase {
+abstract class MyMusicDatabase : RoomDatabase() {
 
     abstract fun queueDao(): QueueDao
 }
